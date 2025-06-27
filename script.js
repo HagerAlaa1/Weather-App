f3d85f19b9f3d91b3576e0a8544eb24b2af70d06
Hager Alaa <hageralaamaher1@gmail.com>
2025-06-27 21:51:30 +0300

    add comment

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const apiUrl = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=
 
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
+
 const weatherIcon = document.querySelector(".weather-icon");
 
 
